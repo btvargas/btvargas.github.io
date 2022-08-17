7fab172ad84ec55e806a5df68ecae885ec104f95
btvargas <93672221+btvargas@users.noreply.github.com>
2022-08-17 13:56:37 -0300

    17 de agos
    17 de agost

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -1,11 +1,4 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
-})
-
-
-
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
     var menu = document.querySelector('.menu');
-    menu.classList.toggle('sticky', window.scrollY > 0);
-  })
+    menu.classList.toggle('sticky', window.scrollY > 100);
+})
